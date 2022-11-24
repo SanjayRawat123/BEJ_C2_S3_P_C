a8da3dad65484e34c2a8bfbefb0db3e5c6e19479
Sanjay <rawatsanjay2799@gmail.com>
2022-11-24 18:19:27 +0530

    final commit

---
+++ src/test/java/com/mongodb/example/BEJ_C2_S3_P_C/BejC2S3PCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodb.example.BEJ_C2_S3_P_C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejC2S3PCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
